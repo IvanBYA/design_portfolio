6efc9223f4cea659bbee7e9e02e27b426d457d4f
IvanBYA <ivan_yakab@hotmail.com>
2021-12-13 19:34:13 -0600

    Seccion Contacto y Footer terminados, archivo CSS sin espacios, Proyecto terminado

--- js/script.js
+++ js/script.js
@@ -1,31 +1,10 @@
-// const navToogle = document.querySelector(".nav-menu");
-// const menu = document.querySelector(".hamburguer");
-
-// menu.addEventListener("click", () => {
-//     navToogle.classList.toggle("nav-toggle");
-// })
-
-
-// var barraHeader = document.querySelector(".barra");
-
-// window.addEventListener("scroll", function(){
-//     if(window.pageYOffset > 300){
-//         barraHeader.classList.add("bck-black");
-//     } else {
-//         barraHeader.classList.remove("bck-black");
-//     }
-// });
-
-
 const navItem = document.querySelector(".nav__item");
 const menuBars = document.querySelector(".menu__desplegable");
 const barraHeader = document.querySelector(".barra");
 
-
 menuBars.addEventListener("click", () => {
     navItem.classList.toggle("nav__item--active");
 })
-
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 400) {
@@ -40,19 +19,12 @@ window.addEventListener("scroll", () => {
 function CambiarBackgroundBarra() {
     barraHeader.classList.add("bg-light");
 }
-
 function RemoverBackgroundBarra() {
     barraHeader.classList.remove("bg-light");
 }
-
 function CambiarColorMenuBar() {
     menuBars.classList.add("color-negro")
 }
-
 function RemoverColorMenuBar() {
     menuBars.classList.remove("color-negro")
 }
-
-
-
-
